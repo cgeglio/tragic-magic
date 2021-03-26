6f7033b3c8eed7297f24db0a7c7b0dd2a3eb9413
Carla Geglio <carla.geglio@gmail.com>
2021-03-26 17:09:30 -0400

    Fix linter errors

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.scss";
 import crystal from "./images/crystal.png";
 
@@ -6,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">Tragic Magic</header>
-      <img src={crystal} height={300} />
+      <img alt="Crystal Ball" src={crystal} height={300} />
     </div>
   );
 }
