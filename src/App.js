f0b2a2aee07fa29635e809f73005065b731c4246
Carla Geglio <carla.geglio@gmail.com>
2021-03-26 14:16:19 -0400

    Add firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.scss";
+import crystal from "./images/crystal.png";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">Tragic Magic</header>
+      <img src={crystal} height={300} />
     </div>
   );
 }
