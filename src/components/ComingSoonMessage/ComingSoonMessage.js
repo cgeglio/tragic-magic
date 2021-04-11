b84284ca230ac1f7deaaf37b45bd73443ac2ac83
Carla Geglio <carla.geglio@gmail.com>
2021-04-11 11:44:23 -0400

    Remove unused class

--- src/components/ComingSoonMessage/ComingSoonMessage.js
+++ src/components/ComingSoonMessage/ComingSoonMessage.js
@@ -4,7 +4,7 @@ import "./comingSoonMessage.scss";
 export default function ComingSoonMessage() {
   return (
     <div className="coming-soon-message-container">
-      <header className="App-header">Tragic Magic</header>
+      <header>Tragic Magic</header>
       <img alt="Crystal Ball" src={crystal} height={300} />
       <div className="coming-soon">Coming Soon!</div>
     </div>
