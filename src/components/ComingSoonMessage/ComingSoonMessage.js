1d6053eaad7070f4819913a0891b302d8e01b4b6
Carla Geglio <carla.geglio@gmail.com>
2021-04-14 21:45:02 -0400

    Remove tragic magic from coming soon msg

--- src/components/ComingSoonMessage/ComingSoonMessage.js
+++ src/components/ComingSoonMessage/ComingSoonMessage.js
@@ -6,7 +6,6 @@ import "./comingSoonMessage.scss";
 export default function ComingSoonMessage() {
   return (
     <div className="coming-soon-message-container">
-      <header>Tragic Magic</header>
       <img alt="Crystal Ball" src={crystal} height={isMobile ? 200 : 300} />
       <div className="coming-soon">Coming Soon!</div>
     </div>
