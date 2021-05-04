656ba9c2930e3b8d13c00bb8b85d9acc988d7054
Carla Geglio <carla.geglio@gmail.com>
2021-05-03 21:10:18 -0400

    Update switch with reading starter

--- src/AppSwitch.js
+++ src/AppSwitch.js
@@ -3,7 +3,7 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import About from "./components/About";
 import Cards from "./components/Cards";
 import Reading from "./components/Reading";
-import ComingSoonMessage from "./components/ComingSoonMessage";
+import ReadingStarter from "./components/ReadingStarter";
 
 export default function AppSwitch() {
   return (
@@ -11,7 +11,7 @@ export default function AppSwitch() {
       <Route path="/about" component={About} />
       <Route path="/cards" component={Cards} />
       <Route path="/reading" component={Reading} />
-      <Route exact path="/" component={ComingSoonMessage} />
+      <Route exact path="/" component={ReadingStarter} />
       <Redirect to="/" />
     </Switch>
   );
