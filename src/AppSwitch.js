662e60d5ca357cd033a3fd575d30f8ce1f6fecc9
Carla Geglio <carla.geglio@gmail.com>
2021-05-03 20:29:51 -0400

    Update app switch routes

--- src/AppSwitch.js
+++ src/AppSwitch.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import About from "./components/About";
-import Embroidery from "./components/Embroidery";
-import Illustration from "./components/Illustration";
+import Cards from "./components/Cards";
+import Reading from "./components/Reading";
 import ComingSoonMessage from "./components/ComingSoonMessage";
 
 export default function AppSwitch() {
   return (
     <Switch>
       <Route path="/about" component={About} />
-      <Route path="/embroidery" component={Embroidery} />
-      <Route path="/illustration" component={Illustration} />
+      <Route path="/cards" component={Cards} />
+      <Route path="/reading" component={Reading} />
       <Route exact path="/" component={ComingSoonMessage} />
       <Redirect to="/" />
     </Switch>
