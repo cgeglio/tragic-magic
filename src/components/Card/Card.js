de20a3aafccd26e43928e2aaa552bdfeee26ba53
Carla Geglio <carla.geglio@gmail.com>
2021-10-21 13:42:52 -0400

    Use modal for card details on mobile

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
-import { Image } from "antd";
+import { Image, Modal } from "antd";
 import cardBack from "../../images/card-back-01.png";
+import useIsMobile from "../../hooks/useIsMobile";
 import "./card.scss";
 
 export default function Card({ card }) {
+  const isMobile = useIsMobile();
+  const [isVisible, setIsVisible] = useState(false);
   const { name, name_short: nameShort, meaning_up: meaning, desc } = card;
+
   const imgSrc = `${process.env.PUBLIC_URL}/cardAssets/${nameShort}.jpg`;
 
   const cardDetails = (
-    <div className="card-details">
+    <div className="card-back-details">
       <div className="card-name">{name}</div>
       <div className="card-category">Meaning:</div>
       <div className="card-description">{meaning}</div>
@@ -18,15 +22,38 @@ export default function Card({ card }) {
     </div>
   );
 
+  const renderCardBack = () => {
+    return (
+      <Image
+        preview={{ mask: cardDetails, maskClassName: "customize-mask" }}
+        src={imgSrc}
+        alt={name}
+      />
+    );
+  };
+
+  const renderMobileCardBack = () => {
+    return (
+      <>
+        <div onClick={() => setIsVisible(true)}>
+          <Image preview={false} src={imgSrc} alt={name} />
+        </div>
+        <Modal
+          visible={isVisible}
+          footer={null}
+          onCancel={() => setIsVisible(false)}
+        >
+          {cardDetails}
+        </Modal>
+      </>
+    );
+  };
+
   return (
     <article className="flip-card fade-in">
       <div className="flip-card-inner">
         <div className="flip-card-back">
-          <Image
-            preview={{ mask: cardDetails, maskClassName: "customize-mask" }}
-            src={imgSrc}
-            alt={name}
-          />
+          {isMobile ? renderMobileCardBack() : renderCardBack()}
         </div>
         <img src={cardBack} alt="Tarot card back" className="flip-card-front" />
       </div>
