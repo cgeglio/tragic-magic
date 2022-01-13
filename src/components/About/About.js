32366a8b9936690378500567bbf15638e66c566b
Carla Geglio <carla.geglio@gmail.com>
2022-01-12 20:04:52 -0500

    Remove pixlee

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,32 +1,19 @@
-import React, { useEffect } from "react";
 import "./about.scss";
 
 export default function About() {
-  useEffect(() => {
-    const scriptTag = document.createElement("script");
-    const feedContainer = document.getElementById("pixlee_container");
-    scriptTag.src =
-      "//instafeed.assets.pxlecdn.com/assets/pixlee_widget_1_0_0.js";
-
-    window.PixleeAsyncInit = () => {
-      // eslint-disable-next-line
-      Pixlee.init({ apiKey: process.env.REACT_APP_PIXLEE_KEY });
-      // eslint-disable-next-line
-      Pixlee.addSimpleWidget({ widgetId: "34712" });
-    };
-
-    feedContainer.style.display = "block";
-    document.body.appendChild(scriptTag);
-
-    return () => {
-      feedContainer.style.display = "none";
-      document.body.removeChild(scriptTag);
-    };
-  }, []);
-
   return (
     <div className="about">
-      <div>Created by Carla Geglio.</div>
+      <div>
+        Created by{" "}
+        <a
+          rel="noopener noreferrer"
+          target="_blank"
+          href="https://www.carlageglio.com/"
+        >
+          Carla Geglio
+        </a>
+        .
+      </div>
       <div>Just for fun.</div>
     </div>
   );
